9d3d29e413fb358542c1bcaff79d9a7f6d5f3eba
Maksim Palashevskiy <maksim.palashevskiy@farmtogether.com>
2021-03-26 01:36:43 +0400

    Aspect logger

---
+++ src/main/kotlin/edu/put_the_machine/scrapper/aspect/ScrapperTimeLoggerAspect.kt
@@ -0,0 +1,21 @@
+package edu.put_the_machine.scrapper.aspect
+
+import org.apache.logging.log4j.LogManager
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class ScrapperTimeLoggerAspect {
+
+    private val log = LogManager.getLogger(ScrapperTimeLoggerAspect::class.java)
+
+    @Around("execution(* edu.put_the_machine.scrapper.service.impl.parsers.sstu.SstuScheduleParser.parse(..))")
+    fun logSstuScheduleParseTime(joinPoint: ProceedingJoinPoint) {
+        val start = System.currentTimeMillis()
+        joinPoint.proceed()
+        log.info("SstuScheduleParser parse time: ${System.currentTimeMillis() - start} millis")
+    }
+}
