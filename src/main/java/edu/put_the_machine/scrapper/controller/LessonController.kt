7fec3069d94f9480012259b74e8ccc6d0fe12653
Dmitry Burnakov <dmgburn@gmail.com>
2021-03-26 08:58:26 +0400

    LaunchScrapper, LessonController fixes, Parser fixes

--- src/main/java/edu/put_the_machine/scrapper/controller/LessonController.kt
+++ src/main/java/edu/put_the_machine/scrapper/controller/LessonController.kt
@@ -4,6 +4,7 @@ import edu.put_the_machine.scrapper.model.dto.LessonDto
 import edu.put_the_machine.scrapper.model.dto.toDto
 import edu.put_the_machine.scrapper.service.LessonService
 import org.springframework.data.domain.Pageable
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -19,8 +20,8 @@ class LessonController(private val lessonService: LessonService) {
         @RequestParam(value = "universityId", required = false) universityId: Long?,
         @RequestParam(value = "teacherId", required = false) teacherId: Long?,
         @RequestParam(value = "groupId", required = false) groupId: Long?,
-        @RequestParam(value = "startDate", required = false) startDate: LocalDate?,
-        @RequestParam(value = "endDate", required = false) endDate: LocalDate?,
+        @DateTimeFormat(pattern = "dd.MM.yyyy") @RequestParam(value = "startDate", required = false) startDate: LocalDate?,
+        @DateTimeFormat(pattern = "dd.MM.yyyy") @RequestParam(value = "endDate", required = false) endDate: LocalDate?,
         @RequestParam(value = "subject", required = false) subject: String?,
         @RequestParam(value = "location", required = false) location: String?,
         @RequestParam(value = "type", required = false) type: String?,
